50c85ccf242742eb2e62608689a864392d153125
david-stewart <david.j.stewart@yale.edu>
2019-01-16 15:37:10 -0500

    Create grace_env.sh

---
+++ grace_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# `source` this script in order to set up the required Python environment to run the ising code.
+module --force purge
+module load StdEnv
+# module load Tools/phys382
+module load Langs/Python/3.6.4
+module load Libs/MATPLOTLIB/1.4.3
